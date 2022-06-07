a01649fd59a49dfae75465a46436ec517ce831a6
gerard2perez <gerard2perez@outlook.com>
2022-06-06 20:37:00 -0500

    class(4): unit testing for person

---
+++ src/app/models/person.mock.ts
@@ -0,0 +1,15 @@
+import { faker } from "@faker-js/faker"
+import { Person } from "./person.model"
+
+export const generateOnePerson = () => {
+  return new Person(
+    faker.name.firstName(),
+    faker.name.lastName(),
+    faker.datatype.number({max: 100}),
+    faker.datatype.number({precision: 0.01, min: 1, max: 200}),
+    faker.datatype.number({precision: 0.01, min: 80, max: 250})
+  )
+}
+export const generateManyPeople = (n: number) => {
+  return Array(n).fill(0).map(_=>generateOnePerson())
+}
