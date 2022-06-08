4f113a39edcb3eee1366b81a7421a613c1d3df6c
gerard2perez <gerard2perez@outlook.com>
2022-06-08 13:30:12 -0500

    class(16): adds async validator

--- src/app/utils/validators.spec.ts
+++ src/app/utils/validators.spec.ts
@@ -1,7 +1,9 @@
 import { FormControl, FormGroup } from "@angular/forms";
+import { mockObservable } from "src/testing";
+import { UsersService } from "../services/user.service";
 import { MyValidators } from "./validators";
 
-fdescribe('Test for my validators', () => {
+describe('Test for my validators', () => {
   describe('test for validPassword', () => {
     it('return null when the password is right', ()=> {
       const control = new FormControl()
@@ -50,6 +52,32 @@ fdescribe('Test for my validators', () => {
       const errorSafe = () => MyValidators.matchPasswords(group)
 
       expect(errorSafe).toThrow(new Error('matchPassword: fields not found'))
+    })
+  })
+  describe('test for validateEmailAsync', () => {
+    it('return null when the email is right', (done)=> {
+      const userService: jasmine.SpyObj<UsersService> = jasmine.createSpyObj('UserService', ['isAvailableByEmail'])
+      userService.isAvailableByEmail.and.returnValue(mockObservable( {isAvailable: true} ))
+      const control = new FormControl()
+      control.setValue('gerardo123')
+
+      const validator = MyValidators.validateEmailAsync(userService)
+      validator(control).subscribe( rta => {
+        expect(rta).toBeNull()
+        done()
+      })
+    })
+    it('returns "{not_available: true}" when email exists', (done)=>{
+      const userService: jasmine.SpyObj<UsersService> = jasmine.createSpyObj('UserService', ['isAvailableByEmail'])
+      userService.isAvailableByEmail.and.returnValue(mockObservable( {isAvailable: false} ))
+      const control = new FormControl()
+      control.setValue('gerardo123')
+
+      const validator = MyValidators.validateEmailAsync(userService)
+      validator(control).subscribe( rta => {
+        expect(rta).toEqual({not_available: true})
+        done()
+      })
     })
   })
 })
